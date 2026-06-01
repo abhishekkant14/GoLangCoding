4e6ac9917d5d38d210ef4419ba81ecac3e2a1b3d
Abhishekkant14 <abhishekkant051@gmail.com>
2026-06-01 21:55:50 +0530

    Add BubbleSort project

---
+++ BubbleSort/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	arr := []int{1, 3, 5, 2, 9, 7}
+
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+
+	}
+	fmt.Println(arr)
+}
