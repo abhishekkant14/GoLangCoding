3e9d1003ece830f5ca8d15d62735239e8ab51ac1
abhishekkant14 <Abhishekkant051@gmail.com>
2026-08-27 18:24:57 +0530

    Updated

--- Count_Vowels/main.go
+++ Count_Vowels/main.go
@@ -1,28 +1,30 @@
+
 package main
 
-import "fmt"
+import(
+	"fmt"
+)
 
-func CountVowels(str string) int {
+func CountVowels(str string)int{
 
-	count := 0
+	count:=0
 
-	for _, ch := range str {
-
-		if ch == 'a' || ch == 'e' || ch == 'i' ||
-			ch == 'o' || ch == 'u' ||
-
-			ch == 'A' || ch == 'E' || ch == 'I' ||
-			ch == 'O' || ch == 'U' {
-			count++
-		}
-	}
-	return count
-
+	for _,ch:=range str{
+if ch=='a' || ch=='e' || ch=='i' ||
+ch=='o' || ch=='u' ||
+ch=='A' || ch=='E' || ch=='I' || 
+ch=='O' || ch=='U'{
+	count++
+}
 }
 
-func main() {
+return count
 
-	fmt.Println(CountVowels("Abhishek"))
-	fmt.Println(CountVowels("Atulesh kant"))
+}
+func main(){
+
+fmt.Println(CountVowels("Abhishek"))
+fmt.Println(CountVowels("Atulesh"))
+
 
 }
