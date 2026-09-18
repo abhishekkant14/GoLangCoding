3c5933ec6bb4bfe447536daaafa17b9d4a604f34
abhishekkant14 <Abhishekkant051@gmail.com>
2026-09-18 06:22:51 +0530

    Update

--- Count_Vowels/main.go
+++ Count_Vowels/main.go
@@ -2,30 +2,29 @@ package main
 
 import "fmt"
 
-func CountBowels(str string) int {
+func CountVowels(str string) int {
 
 	count := 0
 
 	for _, ch := range str {
+
 		if ch == 'a' || ch == 'e' || ch == 'i' ||
-
 			ch == 'o' || ch == 'u' ||
-
 			ch == 'A' || ch == 'E' || ch == 'I' ||
 			ch == 'O' || ch == 'U' {
-
 			count++
-
 		}
 
 	}
 	return count
 
 }
-
 func main() {
 
-	fmt.Println(CountBowels("Abhishek"))
-	fmt.Println(CountBowels("Atulesh"))
+	name1 := "Abhishek"
+	name := "Arpit"
+
+	fmt.Println(CountVowels(name1))
+	fmt.Println(CountVowels(name))
 
 }
